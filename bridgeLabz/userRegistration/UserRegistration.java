092c768e0fcf0f3184f7225c92d4c7734951e032
amruta.vatare <vatareamruta615@gmail.com>
2022-09-12 17:56:38 +0530

    UC4 is implemented

--- bridgeLabz/userRegistration/UserRegistration.java
+++ bridgeLabz/userRegistration/UserRegistration.java
@@ -11,11 +11,17 @@ public class UserRegistration {
     private static final String NAME_PATTERN = "^\\p{Lu}([A-Za-z]{2,})";
     //UC3
     private static final String EMAIL_PATTERN = "^[A-Za-z0-9\\.]+([A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9-]+)+(\\.[A-Za-z0-9-]+)*";
+    //UC4
+    private static final String MOBILE_PATTERN = "^[0-9]{2}\\p{Space}[0-9]{10}";
+
     public void setNamePattern(){
         pattern = Pattern.compile(NAME_PATTERN);
     }
     public void setEmailPattern(){
         pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+    public void setMobilePattern(){
+        pattern = Pattern.compile(MOBILE_PATTERN);
     }
     public boolean validateName(String name){
         setNamePattern();
@@ -25,6 +31,11 @@ public class UserRegistration {
     public boolean validateEmail(String email){
         setEmailPattern();
         matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public boolean validateMobileNo(String mobNo){
+        setMobilePattern();
+        matcher = pattern.matcher(mobNo);
         return matcher.matches();
     }
 
@@ -37,12 +48,22 @@ public class UserRegistration {
         if(!validateName(fName) && validateName(lName)){
             System.out.println("Invalid Fist Name and last both should Start with caps and have minimum 2 characters");
         }
+
         System.out.println("Enter Email_Id");
         String email = sc.next();
         if(!validateEmail(email)){
-            System.out.println("Invalid Email_Id Email should be in format of abc5@gmail.com or abc1.xyz2@yahoo.com.in");
+            System.out.println("Invalid Email_Id, Email should be in format of abc5@gmail.com or abc1.xyz2@yahoo.com.in");
         }
+
+        sc = new Scanner(System.in);
+        System.out.println("Enter mobile no with country code");
+        String mobileNo = sc.nextLine();
+        if(!validateMobileNo(mobileNo)){
+            System.out.println("Invalid mobile_No, Mobile no should start with country code as (country_code space numberHaving10Digit)");
+        }
+
     }
+
 
     public static void main(String[] args) {
         UserRegistration registration = new UserRegistration();
