491b5892ce0a0dce3e2e4366a0cfbba9e49560b5
amruta.vatare <vatareamruta615@gmail.com>
2022-09-14 11:02:29 +0530

    Parameterized password test is implemented of user registration

---
+++ test/java/com/bridgeLabz/userRegistration/PasswordValidationTest.java
@@ -0,0 +1,41 @@
+package com.bridgeLabz.userRegistration;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class PasswordValidationTest {
+    private String password;
+    private boolean result;
+    private UserRegistration passwordValidation;
+
+    public PasswordValidationTest(String password, boolean result){
+        this.password = password;
+        this.result = result;
+    }
+
+    @Before
+    public void beforeEachInit(){
+        passwordValidation = new UserRegistration();
+    }
+    @Parameters
+    public static Collection passwordInputs(){
+        return Arrays.asList(new Object[][]{{"amrutasv",false},{"amrutA",false},
+                {"amrutA9",false},{"amrutA9@",true},
+                {"As@8",false},{"Amr$9uta#",true},
+                {"#As+mr@sd",false},{"#aS%md23sv",true}
+        });
+    }
+    @Test
+    public void validatePassword(){
+        assertEquals(result,passwordValidation.validatePassword(password));
+    }
+}
