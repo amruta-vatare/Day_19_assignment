a94ac8261337308dc663293351ec56794a096638
amruta.vatare <vatareamruta615@gmail.com>
2022-09-14 11:00:33 +0530

    Parameterized Name Test is implemented of user registration

---
+++ test/java/com/bridgeLabz/userRegistration/NameValidationTest.java
@@ -0,0 +1,41 @@
+package com.bridgeLabz.userRegistration;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import org.junit.runners.Parameterized.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class NameValidationTest {
+    private String name;
+    private boolean result;
+    private UserRegistration nameValidation;
+    public NameValidationTest(String name, boolean result){
+        this.name = name;
+        this.result = result;
+    }
+    @Before
+    public void beforeEachInit(){
+        nameValidation = new UserRegistration();
+    }
+
+    @Parameters
+    public static Collection nameInputs(){
+        return Arrays.asList(new Object[][]{{"amRuta",false},{"Ram",true},
+                {"Pu",false},{"Pihu",true},
+                {"Nikita",true},{"12Nikhil",false},
+                {"1233",false},{"Sham12",false},
+                {"Swapnil",true}
+        });
+    }
+    @Test
+    public void validateName(){
+        assertEquals(result,nameValidation.validateName(name));
+    }
+}
